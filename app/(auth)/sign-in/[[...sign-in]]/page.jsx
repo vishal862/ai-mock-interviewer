b9cfe78e1ea5ba72dd2ad636e89812a868af3577
Rajendra Devkate <vishal.rajendra-devkate@capgemini.com>
2025-11-09 15:47:29 +0530

    chnaged the apps name

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -61,7 +61,7 @@ export default function Page() {
           </a>
 
           <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
-            Welcome to Squid 🦑
+            Welcome to AI Interviewer🦑
           </h1>
 
           <p className="mt-4 leading-relaxed text-gray-500">
